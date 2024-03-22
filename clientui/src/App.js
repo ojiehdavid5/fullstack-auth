fd862c9d42d795d589d9789811962a83dc9fdffa
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-03-22 16:44:05 +0100

    first commit

---
+++ clientui/src/App.js
@@ -0,0 +1,14 @@
+// import Register from './Register';
+import Login from './Login'
+
+function App() {
+
+  return (
+    <main className="App">
+      {/* <Register/> */}
+      <Login/>
+    </main>
+  );
+}
+
+export default App;
